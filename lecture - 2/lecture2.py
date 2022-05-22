d1bd96bd941525c37cbcc6747827c1e63348a978
Arnav Parmar <63486503+Arnav12101@users.noreply.github.com>
2022-05-22 15:58:42 +0530

    Create lecture.py

---
+++ lecture - 2/lecture2.py
@@ -0,0 +1,21 @@
+# print("hello")
+
+# r = 10
+# pi = 3.14
+# area = pi*r*r
+# print("Area of circle is",area)
+
+#TAKING INPUT FROM THE USER
+#----------------------------
+# r = input("Enter the radius of the circle: ")#THE VALUE THAT WILL BE TAKEN BY THE USER, WILL BE IN THE FORM OF STRING
+# r = int(input("Enter the radius: "));
+# area = 3.14 * r * r;
+# print("Area",area)
+
+
+# name = input("Enter your name: ")
+# print("Hello,",name)
+
+first = input("Enter first name ");
+second = input("Enter second name ");
+print(first,second);
