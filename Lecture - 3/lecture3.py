4df77f1b186398a3f5ffdc9e3339b981be42e8d7
Arnav Parmar <63486503+Arnav12101@users.noreply.github.com>
2022-05-22 15:59:28 +0530

    Create lecture3.py

---
+++ Lecture - 3/lecture3.py
@@ -0,0 +1,28 @@
+#if-else
+# h = int(input("Enter a number: "))
+# if(h > 10):
+#     print("The number is greater than 10")
+# # elif(h<10):
+# #     print("The number is less than 10")
+# else:
+#     print("equal")
+#
+
+
+#While loop
+#--------------------------------
+# h = int(input("Enter the range: "))
+# while(h<=10):
+#     print(h)
+#     # h=h+1
+#     h+=1
+# print("End of loop")
+
+
+i=1
+while(i<=30):
+    if(i%2 == 0):
+        print("Even",i)
+    elif(i%2!=0):
+        print("Odd",i)
+    i+=1;
